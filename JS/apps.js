3e858b691f23877173f5d13fcb336183330683e1
Tina <tshabestari@my.bcit.ca>
2021-04-04 21:29:08 -0700

    Add the first function for note taking to add and save the notes in javascript.

---
+++ JS/apps.js
@@ -0,0 +1,14 @@
+/* function to add a new note */
+const notes = [
+    {title: 'title'},
+    {body: 'body'}
+]
+//Creating new Array
+function note(push,notesObject) { //Making and declairing new notes by creating a new function
+const notes = []
+for (const notesObject of notes){
+    if (notesObject.status === "new notes"){
+        notes.push(notesObject)
+    }
+ }
+}
