930035ba3cf7d3cf993c6175151e83704906f7e3
Tina <tshabestari@my.bcit.ca>
2021-04-11 20:40:15 -0700

    Accidently delete my pervious work however I restore the function back to javacript file.

--- JS/apps.js
+++ JS/apps.js
@@ -2,29 +2,34 @@
 // Created a new array which we have to objects to present in the js file 
 // were the body and title are object of the notes.
 const totalnotes = []
-
+		
 function note(title,body){
-const notes = [
-    {title: 'title'},
-    {body: 'body'}
- ]
+		const notes = [
+		    {title: 'title'},
+		    {body: 'body'}
+		 ]
+        
 }
 //Creating new Array
 function note(push,notesObject) { //Making and declairing new notes by creating a new function
 const notes = []
 for (const notesObject of notes){
-    if (notesObject.status === "new notes"){
-        notes.push(notesObject)
-    }
- }
+	if (notesObject.status === "new notes"){
+		notes.push(notesObject)
+		}
+	}
 }
 // Creating second function which included a querySelector as well as add event Listerner too.
 const firstnote = document.querySelector('#notes');
 const output = document.querySelector('.notes-output');
+output.textContent = notes.value;
 output.textContent = totalnotes.value;
 noteone.addEventLisner('input',function(){
-    output.textContent = totalnotes.value;
-});
+		    output.textContent = notes.value;
+		    output.textContent = totalnotes.value;
+}); 
+
+
 
 console.log('Welcome to note app. This is app.js');
 
